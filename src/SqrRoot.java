8fbe3296849d278240d86d4a609fd0ff949242dc
alex282-prog <06eOr8tg1EZH>
2019-11-11 23:30:48 +0300

    probo

--- src/SqrRoot.java
+++ src/SqrRoot.java
@@ -4,7 +4,7 @@ public class SqrRoot {
         for(num = 1.0;num > -100.0; num-=10){
             sroot = Math.sqrt(num);
             System.out.println("Корено квадрата из " + num + " равен: " + sroot);
-            // Вычислить ошибку округлени
+            // Вычислить ошибку округления
             rerr = num - (sroot * sroot);
             System.out.println("Ошибка округления " + rerr + "\n");
         }
